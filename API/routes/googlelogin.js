0e62ba4de42382bdf2045b52fbaaedf4eed57b4c
Mihir Karandikar <mihirkkarandikar@gmail.com>
2022-08-24 18:27:40 +0530

    update deprecated google login

--- API/routes/googlelogin.js
+++ API/routes/googlelogin.js
@@ -8,10 +8,10 @@ const { genToken } = require('../middleware/auth');
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 route.post('/', (req, res, next) => {
-    const { googleToken } = req.body;
+    const { name , email } = req.body;
     
-    async function foundAndSend(user,imageUrl) {
-        let user1 = { ...user._doc , imageUrl : imageUrl };
+    async function foundAndSend(user) {
+        let user1 = { ...user._doc};
         await Item.find({ _id: { $in: user.favourites } })
             .select('price title images')
             .sort({ date: 'desc' })
@@ -37,28 +37,15 @@ route.post('/', (req, res, next) => {
         res.status(200).send({ user: user, accessToken });
     };
 
-    //Main function
-    client.verifyIdToken({ idToken: googleToken, audience: process.env.GOOGLE_CLIENT_ID })
-        .then(res => {
-            const { email_verified, name, email , imageUrl } = res.payload;
-            console.log("verifying profile",imageUrl)
-
-            if (email_verified) {
-                User.findOne({ email })
-                    .then((user) => {
-                        if (user) {
-                            foundAndSend(user,imageUrl);
-                        } else {
-                            User.create({ name, email , imageUrl})
-                                .then(user => {
-                                    createdAndSend(user);
-                                })
-                        }
+    User.findOne({ email })
+        .then((user) => {
+            if (user) {
+                foundAndSend(user);
+            } else {
+                User.create({ name, email})
+                    .then(user => {
+                        createdAndSend(user);
                     })
-                    .catch(next);
-            }
-            else {
-                next();
             }
         })
         .catch(next);
